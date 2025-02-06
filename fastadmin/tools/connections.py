d5f75e2060ec234e62b04654946c52ee28f88fe5
Valerii <valera.dchn@gmail.com>
2025-02-06 21:12:12 +0200

    feat: connection manager

--- fastadmin/tools/connections.py
+++ fastadmin/tools/connections.py
@@ -1,6 +1,5 @@
-from contextlib import contextmanager
-
-from typing import TypeVar, Generic, Iterable
+from contextlib import contextmanager, asynccontextmanager
+from typing import TypeVar, Generic
 
 from sqlalchemy import Engine, Connection
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncConnection
@@ -18,49 +17,58 @@ class ConnectionMeta(type):
 _T = TypeVar("_T", AsyncConnection, Connection)
 _E = TypeVar("_E", Engine, AsyncEngine)
 
-
-class ConnectionSet(set, Generic[_T, _E]):
-    def __init__(self, iterable: Iterable[_T], /, engine: _E):
-        super(ConnectionSet, self).__init__(iterable)
+# TODO: tests
+class ConnectionABS(Generic[_T, _E]):
+    def __init__(self, engine: _E):
+        self.connections: set[_T] = set()
         self.engine = engine
 
     @property
     def empty(self) -> bool:
-        return len(self) == 0
-
-    def new(self) -> _T:
-        return self.engine.connect()
-
-    def get(self) -> _T:
-        if self.empty:
-            return self.new()
-
-        conn = self.pop()  # noqa F841
-
-
-class ConnectionABS(Generic[_T, _E]):
-    def __init__(self, engine: _E):
-        self.connections: ConnectionSet[_T, _E] = ConnectionSet(engine=engine)
+        return not self.connections
+    
+    def new_connection(self) -> _T:
+        conn = self.engine.connect()
+        self.add(conn)
+        return conn
 
     def add(self, conn: _T) -> None:
-        self.connections.add(conn)
+        if conn not in self.connections:
+            self.connections.add(conn)
 
+    def get_connection(self) -> _T:
+        if self.empty:
+            return self.new_connection()
+        
+        for conn in self.connections:
+            if conn.closed:
+                self.connections.remove(conn)
+            return conn
+        return self.new_connection()
+    
     @contextmanager
     def connection(self):
-        conn = self.connections.get()
+        conn = self.get_connection()
         try:
             yield conn
         finally:
-            if not conn.closed:
-                self.connections.add(conn)
-
+            if conn.closed:
+                self.connections.remove(conn)
 
 class ConnectionRegistry(ConnectionABS[Connection, Engine]):
-    pass
+    
+    def close_all(self):
+        for conn in self.connections:
+            if not conn.closed:
+                conn.close()
 
 
 class AsyncConnectionRegistry(ConnectionABS[AsyncConnection, AsyncEngine]):
-    pass
+    
+    async def close_all(self):
+        for conn in self.connections:
+            if not conn.closed:
+                await conn.close()
 
 
 class ConnectionManager(metaclass=ConnectionMeta):
@@ -72,3 +80,59 @@ class ConnectionManager(metaclass=ConnectionMeta):
 
         self.registry = ConnectionRegistry(engine)
         self.async_registry = AsyncConnectionRegistry(aengine)
+
+    @contextmanager
+    def connection(
+        self, *, close_after: bool = True, commit: bool = False
+    ):
+        with self.registry.connection() as conn:
+            try:
+                yield conn
+            except Exception as e:
+                if conn.in_transaction():
+                    conn.rollback()
+                raise e
+            else:
+                if commit and not conn.in_transaction():
+                    conn.commit()
+            finally:
+                if close_after and not conn.closed:
+                    conn.close()
+
+    @asynccontextmanager    
+    async def aconnection(
+        self, *, close_after: bool = True, commit: bool = False
+    ):
+        with self.async_registry.connection() as conn:
+            try:
+                yield conn
+            except Exception as e:
+                if conn.in_transaction():
+                    await conn.rollback()
+                raise e
+            else:
+                if commit and not conn.in_transaction():
+                    await conn.commit()
+            finally:
+                if close_after and not conn.closed:
+                    await conn.close()
+
+
+def with_connection(func=None, *, close_after: bool = True, commit: bool = False):
+    def decorator(func): 
+        def wrapper(*args, **kwds):
+            conn_manager = ConnectionManager()
+            with conn_manager.connection(close_after=close_after, commit=commit) as conn:
+                return func(*args, connection=conn, **kwds)
+        return wrapper
+    return decorator(func) if callable(func) else decorator
+
+
+def with_aconnection(func=None, *, close_after: bool = True, commit: bool = False):
+    def decorator(func):
+        async def wrapper(*args, **kwds):
+            conn_manager = ConnectionManager()
+            async with conn_manager.aconnection(close_after=close_after, commit=commit) as conn:
+                return await func(*args, connection=conn, **kwds)
+        return wrapper
+    return decorator(func) if func else decorator
